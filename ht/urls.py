f80da829cdade00534381896a38fa4383f43c31e
a <a.a@a.com>
2018-10-27 22:36:12 +0300

    Add logout, registration page

--- ht/urls.py
+++ ht/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
-    path('main_app/', include('main_app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('main_app.urls'))
+    path('main_app/', include('main_app.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
