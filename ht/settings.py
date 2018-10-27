07a39c1bd6fbc40b11c061f2a3d03b3efe3f1c40
Maria <mariaprotas@yandex.ru>
2018-10-27 14:30:48 +0300

    postgres db setings set

--- ht/settings.py
+++ ht/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's4z3q3zkr8$koiv@zn+#!69v0=-hlf1m0zi_vnsq30s7(=x2r^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ht.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+	'USER': 'hacker', 
+	'PASSWORD': 'pass', 
+	'HOST': '127.0.0.1', 
+	'PORT': '5432',
     }
 }
 
