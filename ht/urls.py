e14373e49feed98d7f918f21e6f9105bbfa49402
a <a.a@a.com>
2018-10-27 19:41:14 +0300

    Add login page

--- ht/urls.py
+++ ht/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('main_app/', include('main_app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('main_app.urls'))
 ]
