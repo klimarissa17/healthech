4d682c089d783b7332abb84f652e107a34b9be07
Andrew <andrew-199@ya.ru>
2018-10-27 18:55:21 +0300

    address fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='hello'),
-    url(r'drugs_list', views.drugs_list, name='drugs_list')
+    url(r'^drugs_list/?$', views.drugs_list, name='drugs_list')
 ]
