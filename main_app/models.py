ca42dfb6b5392a9bda590cc47f3e5e77e6fe5cab
Andrew <andrew-199@ya.ru>
2018-10-27 14:23:12 +0300

    Add db models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+class Drugs(models.Model):
+    name = models.TextField()
+    active_substance = models.TextField()
+    indications = models.TextField()
+    contraindications = models.TextField()
+    mode_of_application = models.TextField()
+
+
+class Diseases(models.Model):
+    name = models.TextField()
+    origin = models.TextField()
+    forecast = models.TextField()
+    drug_category = models.TextField()
+
+
+class Patients(models.Model):
+    name = models.TextField()
+    age = models.IntegerField()
+    sex = models.TextField()
+    specificities = models.TextField()
+
+
+class Doctors(models.Model):
+    name = models.TextField()
+    age = models.IntegerField()
+    sex = models.TextField()
+    specificities = models.TextField()
+
+
+class Receipt(models.Model):
+    doctor_id = models.ForeignKey(Doctors, on_delete=models.CASCADE)
+    patient_id = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    disease_id = models.ForeignKey(Diseases, on_delete=models.CASCADE)
+
+
+class DrugsByReceipt(models.Model):
+    receipt_id = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    drug_id = models.ForeignKey(Drugs, on_delete=models.CASCADE)
+
+
+class PatientsDiseases(models.Model):
+    patient_id = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    disease_id = models.ForeignKey(Diseases, on_delete=models.CASCADE)
+
