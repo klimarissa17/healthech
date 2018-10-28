afa8656de22188ef389cffa63250a4148096b25a
Sam Kahr <samkahr@gmail.com>
2018-10-28 03:51:46 +0300

    adding new login code that will actually let you log in

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
-from . import views 
+from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-<<<<<<< HEAD
-    url(r'^drugs_list/?$', views.drugs_list, name='drugs_list')
-=======
     url(r'^home/?$', views.home, name='home'),
     url(r'^drugs_list/?$', views.drugs_list, name='drugs_list'),
->>>>>>> 9ad06c43e088739c7e10b2a397c9abb1d1c468c1
 ]
